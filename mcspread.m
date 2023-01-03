17e2d60339dd1cc87c6e74c215deb9c61a6c5b77
juigmend <juigmend@jyu.fi>
2023-01-03 03:03:19 +0200

    Replace mcspread.m

--- mcspread.m
+++ mcspread.m
@@ -1,72 +1,45 @@
-function s = mcspread(d,varargin)
-% Sum of inter-marker distances per time-window.
+function s = mcspread(d)
+% Sum of inter-marker distances per frame.
 %
 % syntax:
 %   s = mcspread(d);
-%   s = mcspread(d,w,hop);
-%   s = mcspread(d,w,hop,dim);
 %
 % input:
-%   d: MoCap structure
-%   w: window length in frames (optional, default = 1)
-%   hop: window hop in frames (optional, default = 1)
-%   dim: dimensions, logical [x,y,z] (optional, default = [1,1,1])
+%   d: MoCap structure (*) 
+%      or matrix where rows are frames and columns are xyz dimensions of markers
+%      e.g., marker_1_x, marker_1_y, marker_1_z, marker_2_x, marker_2_y, etc.
 %
 % output:
-%   s: spread time-series
+%   s: spread time-series (or scalar if only one frame)
 %
-% note:
-%   This function used on a plane (i.e., two non-zero dim) may give a more 
-%   accurate estimation of the change in area covered than function mcboundrect.
+% (*) see the MoCap toolbox:
+% https://www.jyu.fi/hytk/fi/laitokset/mutku/en/research/materials/mocaptoolbox
 %
-% VERSION: 5 June 2021
+% VERSION: 2 January 2023
 %
 % Juan Ignacio Mendoza
 % University of Jyväskylä
 
-n_dims = 3;
-dims = 1:3;
-
-if nargin == 1
+if ~isstruct(d)
     
-    w = 1;
-    hop = 1;
+    d_tmp = d;
+    clear d
+    d.data = d_tmp;
+    [d.nFrames,n_cols] = size(d.data);
+    d.nMarkers = n_cols / 3;
 else
-    
-    w = varargin{1};
-    hop = varargin{2};
-    
-    if nargin == 4 % dim
-        
-        if ~isvector(varargin{3}) || (length(varargin{3})~=3) || any(varargin{3}>1) || any(varargin{3}<0)
-            error('third argument (dim) should be a vector of 3 boolean values')
-        end
-        n_dims = sum(varargin{3});
-        dims = varargin{3}.*(1:3);
-        dims = dims(dims>0);
-    end
+    n_cols = d.nMarkers * 3;
 end
     
-n_w = floor((d.nFrames-(w))/hop)+1;
-s_w = d.nMarkers * w;
+pdists = zeros(d.nFrames, (d.nMarkers^2-d.nMarkers)/2) ;
 
-pdists = NaN(n_w, (s_w^2-s_w)/2);
-these_vals = zeros(s_w,n_dims);
-
-w_m = w-1;
-w_start = 1;
-for i_w = 1:n_w
+for i_frame = 1:d.nFrames
     
-    w_end = w_start + w_m;
-    this_w = d.data(w_start:w_end,:);
-
-    for i_dim = 1:n_dims
-        this_w_dim = this_w(:,dims(i_dim):3:end);
-        these_vals(:,i_dim) = this_w_dim(:); 
-    end
+    this_frame(1:d.nMarkers,1) = d.data(i_frame,1:3:n_cols); % x
+    this_frame(1:d.nMarkers,2) = d.data(i_frame,2:3:n_cols); % y
+    this_frame(1:d.nMarkers,3) = d.data(i_frame,3:3:n_cols); % z
     
-    pdists(i_w,:) = pdist(these_vals);
-    w_start = w_start + hop;
+    pdists(i_frame,:) = pdist(this_frame);
 end
 
 s = sum(pdists,2);
